5fa17f0759b67d2246276362937f5b616981b354
Lim95 <73658212+Lim98SE@users.noreply.github.com>
2022-08-05 16:24:11 -0600

    Update DoubleVisual.py

--- DoubleVisual.py
+++ DoubleVisual.py
@@ -1,5 +1,4 @@
-# DGR-2
-# memory range is (0, 0) - (255, 240)
+# DGR-3
 
 import pygame
 import os
@@ -8,26 +7,33 @@ global data
 
 pygame.init()
 
-draw = pygame.Surface((255, 240))
-screen = pygame.display.set_mode((255*3,240*3))
+res = 64
+scale = 10
+
+screen = pygame.display.set_mode((res*scale,res*scale))
 pygame.display.set_caption("Double Screen")
 
 pal = []
 
-for B in range(3):
-    for G in range(3):
-        for R in range(3):
-            pal.append((R * (255 // 2), G * (255 // 2), B * (255 // 2))) # Generates the pallate lmao
+depth = 4
+
+for B in range(depth):
+    for G in range(depth):
+        for R in range(depth):
+            pal.append((R * (255 // depth - 1), G * (255 // depth - 1), B * (255 // depth - 1))) # Generates the pallate lmao
 
 def tickScreen(data):
+
+    draw = pygame.Surface((res, res))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
-    for Y in range(240):
-        for X in range(255):
+    for Y in range(res):
+        for X in range(res):
             draw.set_at((X, Y), pal[data[X][Y]%len(pal)])
 
-    screen.blit(pygame.transform.scale(draw, screen.get_rect().size), (0,0)) # big
+    screen.blit(pygame.transform.scale(draw, (res*scale, res*scale)), (0,0)) # big
     
     pygame.display.update()
