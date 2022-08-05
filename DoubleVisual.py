edcff40511d09f9f8bf42f4fab8b5a01a75a3bba
Lim95 <73658212+Lim98SE@users.noreply.github.com>
2022-08-05 15:35:59 -0600

    Update DoubleVisual.py

--- DoubleVisual.py
+++ DoubleVisual.py
@@ -1,4 +1,5 @@
-# DGR-1
+# DGR-2
+# memory range is (0, 0) - (255, 240)
 
 import pygame
 import os
@@ -7,26 +8,26 @@ global data
 
 pygame.init()
 
-draw = pygame.Surface((256//4, 256//4))
-screen = pygame.display.set_mode((256*3,256*3))
+draw = pygame.Surface((255, 240))
+screen = pygame.display.set_mode((255*3,240*3))
 pygame.display.set_caption("Double Screen")
 
 pal = []
 
-for R in range(5):
-    for G in range(5):
-        for B in range(5):
-            pal.append((R * 63, G * 63, B * 63))
+for B in range(3):
+    for G in range(3):
+        for R in range(3):
+            pal.append((R * (255 // 2), G * (255 // 2), B * (255 // 2))) # Generates the pallate lmao
 
 def tickScreen(data):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
-    for Y in range(255//4):
-        for X in range(255//4):
+    for Y in range(240):
+        for X in range(255):
             draw.set_at((X, Y), pal[data[X][Y]%len(pal)])
 
-    screen.blit(pygame.transform.scale(draw, screen.get_rect().size), (0,0))
+    screen.blit(pygame.transform.scale(draw, screen.get_rect().size), (0,0)) # big
     
     pygame.display.update()
