9c12f835e53b7a72044a8928837af1c739c33b81
Lim95 <73658212+Lim98SE@users.noreply.github.com>
2022-09-10 13:21:12 -0600

    added variables :)

--- Assembler.py
+++ Assembler.py
@@ -1,3 +1,5 @@
+# assembler update?????
+
 import sys
 
 file = sys.argv[1]
@@ -9,22 +11,20 @@ code = code.upper()
 code = code.strip()
 code = code.split()
 
-labels={}
+labels = {}
+variables = {}
 
 def generateDouble(text):
-    output = ""
+    output = "DB "
     for i in text:
-        h = hex(ord(i))[2:5]
-        if len(h) == 1:
-            output += f"SV 0{h} PC "
-        else:
-            output += f"SV {h} PC "
+        output += f"{ord(i)} "
+    output += f"{0xFF}"
     return output
 
 for i in code:
     if i[len(i)-1] == ":":
         if len(hex(code.index(i))[2:5]) == 1:
-            labels[i[:len(i)-1]] = f"0{hex(code.index(i))[2:5]}"
+            labels[i[:len(i)-1]] = f"code.index(i)"
         else:
             labels[i[:len(i)-1]] = hex(code.index(i))[2:5]
         code.pop(code.index(i))
@@ -41,16 +41,24 @@ for i in code:
         else:
             i = i.replace(f"#{char}",h)
         code[index] = i
+    if i[0] == "$":
+        if "=" in i:
+            c = i.split("=")
+            variables[c[0]] = c[1]
 
-print(labels)
+print(labels, variables)
 
 labeled_code = []
 
 for i in code:
     if i in labels:
         labeled_code.append(labels[i])
+    elif i in variables:
+        labeled_code.append(variables[i])
     else:
         labeled_code.append(i)
 
 out = open(output,"w")
 out.write(" ".join(labeled_code))
+
+print("Make sure to remove variable definitions!")
